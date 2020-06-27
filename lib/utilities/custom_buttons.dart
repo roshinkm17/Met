86519ed05cb2913670d42e9eb8bc6c95262d9550
roshinkm17 <60546484+roshinkm17@users.noreply.github.com>
2020-06-27 21:20:59 +0530

    -logo image added in /images -pubspec updated -sign-up and sign-in buttons made

--- lib/utilities/custom_buttons.dart
+++ lib/utilities/custom_buttons.dart
@@ -1,19 +1,56 @@
 import 'package:flutter/material.dart';
 
+class SignInButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 40, vertical: 5),
+      width: double.infinity,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [Colors.orangeAccent, Colors.deepOrange]),
+        borderRadius: BorderRadius.circular(50),
+      ),
+      padding: EdgeInsets.symmetric(horizontal: 25, vertical: 5),
+      child: RawMaterialButton(
+        onPressed: null,
+        child: Text(
+          "Sign In",
+          style: TextStyle(
+            fontWeight: FontWeight.w300,
+            fontSize: 20,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 class SignUpButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.black87,
-      decoration: BoxDecoration(),
-      padding: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+      margin: EdgeInsets.symmetric(horizontal: 40, vertical: 5),
+      width: double.infinity,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(50),
+        border: Border.all(
+          color: Colors.deepOrangeAccent,
+          width: 2,
+        ),
+      ),
+      padding: EdgeInsets.symmetric(horizontal: 25, vertical: 5),
       child: RawMaterialButton(
         onPressed: null,
         child: Text(
           "Sign Up",
           style: TextStyle(
-            fontSize: 30,
-            color: Colors.deepOrangeAccent,
+            fontWeight: FontWeight.w300,
+            fontSize: 20,
+            color: Colors.deepOrange,
           ),
         ),
       ),

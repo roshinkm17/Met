e4cec73852fa1d94d260a945ea054312b8bdfb44
roshinkm17 <60546484+roshinkm17@users.noreply.github.com>
2020-06-27 20:17:02 +0530

    i little initialization

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: Scaffold(
-        body: SafeArea(child: Container()),
+        body: SafeArea(),
       ),
     );
   }

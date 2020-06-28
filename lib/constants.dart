8a6761d37753b455de5f0107ab903595635bc4db
roshinkm17 <60546484+roshinkm17@users.noreply.github.com>
2020-06-28 20:27:40 +0530

    final for phase 1

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,3 +4,4 @@ const TextStyle textStyle = TextStyle(
   fontWeight: FontWeight.w300,
   fontSize: 20,
 );
+//const Color transparent = Color(0x00000000);
--- lib/screens/upload_file_screen.dart
+++ lib/screens/upload_file_screen.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'package:met/utilities/home_screen_icon_cards.dart';
 import 'package:path/path.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -21,6 +22,7 @@ class _UploadScreenState extends State<UploadScreen> {
     getCurrentUser();
   }
 
+  String _uploadedFileURL;
   FirebaseUser currentUser;
   String currentUserEmail;
   void getCurrentUser() async {
@@ -30,78 +32,205 @@ class _UploadScreenState extends State<UploadScreen> {
     });
   }
 
+  void onTabTapped(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
+  }
+
   File file;
   bool _isSaving = false;
+  int selectedIndex = 0;
+  bool fileSelected = false;
   final FirebaseAuth _auth = FirebaseAuth.instance;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        showUnselectedLabels: false,
+        fixedColor: Colors.deepOrange,
+        unselectedItemColor: Colors.black54,
+        currentIndex: selectedIndex,
+        onTap: onTabTapped,
+        items: [
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.home),
+            title: new Text('Home'),
+          ),
+          BottomNavigationBarItem(
+            icon: new Icon(FontAwesomeIcons.heart),
+            title: new Text('Messages'),
+          ),
+          BottomNavigationBarItem(
+            icon: new Icon(FontAwesomeIcons.search),
+            title: new Text('Search'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            title: Text('Profile'),
+          )
+        ],
+      ),
       body: ModalProgressHUD(
         inAsyncCall: _isSaving,
         progressIndicator: CircularProgressIndicator(),
-        child: Container(
-          width: double.infinity,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Text(currentUserEmail),
-              Text("Upload Screen"),
-              Container(
-                color: Colors.orangeAccent.shade200,
-                margin: EdgeInsets.symmetric(vertical: 20),
-                child: RawMaterialButton(
-                  child: Text("Select file"),
-                  onPressed: () async {
-                    file = await FilePicker.getFile();
-                  },
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        CircleAvatar(
+                          radius: 20,
+                          child: Icon(
+                            Icons.person,
+                            color: Colors.white,
+                          ),
+                          backgroundColor: Colors.black87,
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(left: 10),
+                          child: Text(
+                            "John Doe",
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Icon(FontAwesomeIcons.qrcode)
+                  ],
                 ),
-              ),
-              RawMaterialButton(
-                onPressed: () async {
-                  setState(() {
-                    _isSaving = true;
-                  });
-                  try {
-                    StorageReference storageReference = FirebaseStorage()
-                        .ref()
-                        .child("$currentUserEmail/${basename(file.path)}");
-                    StorageUploadTask uploadTask =
-                        storageReference.putFile(file);
-                    await uploadTask.onComplete;
-                    StatusAlert.show(context,
-                        duration: Duration(seconds: 2),
-                        title: "File Uploaded Successfully",
-                        configuration: IconConfiguration(icon: Icons.check),
-                        );
-                    setState(() {
-                      _isSaving = false;
-                    });
-                  } catch (e) {
-                    print(e);
-                    setState(() {
-                      _isSaving = false;
-                    });
-                  }
-                },
-                child: Text("Upload"),
-              ),
-              GestureDetector(
-                onTap: () async {
-                  await _auth.signOut();
-                  Navigator.pop(context);
-                },
-                child: Container(
-                  margin: EdgeInsets.all(10),
+                SizedBox(height: 40),
+                Text(
+                  "Hi, John \nComplete you profile..",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w300),
+                  textAlign: TextAlign.start,
+                ),
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    HomeScreenIconCard(FontAwesomeIcons.fileAlt, "Upload Docs"),
+                    HomeScreenIconCard(
+                        FontAwesomeIcons.addressBook,
+                        "Allow "
+                        "contacts"),
+                    HomeScreenIconCard(
+                        FontAwesomeIcons.qrcode,
+                        "Setup QR "
+                        "Code"),
+                  ],
+                ),
+                SizedBox(height: 30),
+                Container(
                   padding: EdgeInsets.all(20),
-                  color: Colors.black54,
-                  child: Icon(
-                    FontAwesomeIcons.signOutAlt,
-                    color: Colors.white,
+                  decoration: BoxDecoration(
+                    color: Colors.deepOrangeAccent,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  width: double.infinity,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Icon(
+                        FontAwesomeIcons.filePdf,
+                        color: Colors.white,
+                      ),
+                      Text(
+                        "Upload your Doc",
+                        style: TextStyle(color: Colors.white, fontSize: 16),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          Container(
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: fileSelected == false
+                                  ? Colors.white
+                                  : Colors.green,
+                            ),
+                            child: RawMaterialButton(
+                              splashColor: Colors.transparent,
+                              onPressed: () async {
+                                file = await FilePicker.getFile();
+                                setState(() {
+                                  fileSelected = true;
+                                });
+                              },
+                              child: Icon(
+                                fileSelected == false ? Icons.add : Icons.check,
+                                size: 16,
+                                color: fileSelected == false
+                                    ? Colors.orangeAccent.shade700
+                                    : Colors.white,
+                              ),
+                            ),
+                          ),
+                          Container(
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.white,
+                            ),
+                            child: RawMaterialButton(
+                                splashColor: Colors.transparent,
+                                shape: CircleBorder(),
+                                padding: EdgeInsets.all(0),
+                                onPressed: () async {
+                                  setState(() {
+                                    _isSaving = true;
+                                  });
+                                  try {
+                                    StorageReference storageReference =
+                                        FirebaseStorage().ref().child(
+                                            "$currentUserEmail/${basename(file.path)}");
+                                    StorageUploadTask uploadTask =
+                                        storageReference.putFile(file);
+                                    await uploadTask.onComplete;
+                                    StatusAlert.show(
+                                      context,
+                                      duration: Duration(seconds: 2),
+                                      title: "File Uploaded Successfully",
+                                      configuration: IconConfiguration(
+                                        icon: Icons.check,
+                                      ),
+                                    );
+                                    storageReference
+                                        .getDownloadURL()
+                                        .then((fileURL) {
+                                      setState(() {
+                                        _uploadedFileURL = fileURL;
+                                        print(_uploadedFileURL);
+                                        _isSaving = false;
+                                        fileSelected = false;
+                                      });
+                                    });
+                                  } catch (e) {
+                                    print(e);
+                                    setState(() {
+                                      _isSaving = false;
+                                    });
+                                  }
+                                },
+                                child: Icon(
+                                  FontAwesomeIcons.arrowUp,
+                                  size: 14,
+                                  color: Colors.orangeAccent.shade700,
+                                )),
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
